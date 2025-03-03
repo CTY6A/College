319b2625911924efa1a7fb72f2fc0426c652cc84
CTY6A <stubeda0002@mail.ru>
2025-03-03 23:24:19 +0500

    Upload second year

---
+++ Second-year/Object-oriented-programming-technologies-and-design-standards/CRUD_WithSereializer/WindowsFormsApp1/z7.cs
@@ -0,0 +1,55 @@
+﻿namespace CRUD
+{
+    public interface ISerializerFactory
+    {
+        Serializer CreateSerializer();
+    }
+    public class JsonFactory : ISerializerFactory
+    {
+        public JsonFactory(string name)
+        {
+            Name = name;
+        }
+        public Serializer CreateSerializer()
+        {
+            return new JsonSerializer();
+        }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    public class BinaryFactory : ISerializerFactory
+    {
+        public BinaryFactory(string name)
+        {
+            Name = name;
+        }
+        public Serializer CreateSerializer()
+        {
+            return new BinarySerializer();
+        }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    public class ClientFactory : ISerializerFactory
+    {
+        public ClientFactory(string name)
+        {
+            Name = name;
+        }
+        public Serializer CreateSerializer()
+        {
+            return new ClientSerializer();
+        }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
